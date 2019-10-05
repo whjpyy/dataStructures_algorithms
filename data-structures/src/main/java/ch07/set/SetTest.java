b5d17c2b0d8d5a7bc650704e24fc45f213778c79
chen <409121961@qq.com>
2019-10-05 21:11:39 +0800

    玩转数据结构：7-3.基于链表的集合实现.

---
+++ data-structures/src/main/java/ch07/set/SetTest.java
@@ -0,0 +1,31 @@
+package ch07.set;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetTest {
+
+    private static double testSet(Set<String> set, List<String> words){
+        long startTIme = System.nanoTime();
+        System.out.println("Total words: " + words.size());
+        for (String word : words) {
+            set.add(word);
+        }
+        System.out.println("Total differrent words: " + set.getSize());
+        return (System.nanoTime() - startTIme) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        FileOperation.readFile("spring-core.txt", words);
+
+        Set<String> bstSet = new BSTSet<>();
+        double time1 = testSet(bstSet, words);
+        System.out.println("BST set: " + time1 + "s");
+        System.out.println();
+        Set<String> linkedListSet = new LinkedListSet<>();
+        double time2 = testSet(linkedListSet, words);
+        System.out.println("LinkedListSet set: " + time2 + "s");
+
+    }
+}
