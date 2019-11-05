d23b759b8b2e7610afb375bc238d10892d97756a
chen <409121961@qq.com>
2019-11-05 23:04:55 +0800

    玩转数据结构：12.AVL。

---
+++ data-structures/src/main/java/ch12/avltree/Test.java
@@ -0,0 +1,45 @@
+package ch12.avltree;
+
+import ch07.set.BSTMap;
+import ch07.set.FileOperation;
+import ch07.set.Map;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        if(FileOperation.readFile("TheUnadjustedGirl.txt",words)){
+//            Collections.sort(words);
+
+            long startTime = System.nanoTime();
+
+            Map<String, Integer> bst = new BSTMap<>();
+            for (String word : words) {
+                if(bst.contains(word)){
+                    bst.set(word, bst.get(word) + 1);
+                }else{
+                    bst.add(word, 1);
+                }
+            }
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("BST: " + time + "s");
+
+            startTime = System.nanoTime();
+            AVLTree<String, Integer> avlTree = new AVLTree<>();
+            for (String word : words) {
+                if(avlTree.contains(word)){
+                    avlTree.set(word, avlTree.get(word) + 1);
+                }else{
+                    avlTree.add(word, 1);
+                }
+            }
+            endTime = System.nanoTime();
+            time = (endTime - startTime) / 1000000000.0;
+            System.out.println("AVL: " + time + "s");
+        }
+    }
+}
